16bb8d1d3dea1de4ca9a4d550840379f24bb967d
luljn <mbohlulajonathan4@gmail.com>
2024-07-11 22:25:00 +0200

    Global style changed, some routes have been added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HeaderComponent,
+    SharedModule
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
 export class CoreModule { }
