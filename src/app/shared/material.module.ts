e82bad188b4200fa73aece86aad68407a0cd5c2c
luljn <mbohlulajonathan4@gmail.com>
2024-07-14 12:56:08 +0200

    MaterialModule added

---
+++ src/app/shared/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+    exports: [
+        MatToolbarModule,
+        MatCardModule
+    ]
+})
+export class MaterialModule {}
